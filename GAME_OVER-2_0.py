8c1e56be187b3f7258e8a3db4230b1d7eacd50a4
Tyrah <47400063+ursaturnine@users.noreply.github.com>
2021-01-08 14:09:51 -0800

    uploading my "game over" code
    python practice with strings, triple quoted strings, formatting, comments, intro to ascii etc. 
    (I need to re-do formatting);

---
+++ GAME_OVER-2_0.py
@@ -0,0 +1,33 @@
+# Game Over - Version 2]
+# Demonstrates the use of quotes in strings
+
+print ("Program 'Game Over' 2.0")
+
+print ("Same", "message", "as before")
+
+print ("Just",
+       "a bit",
+       "bigger")
+print ("Here", end=" ")
+print ("it is...")
+
+print (
+        """
+        _____     ____        __   __     _____
+       /     |   /     |     /  \ /  \   |   __|
+          ____  /           /        |   |  |_ 
+       |  |    /  / |  |   /  /|  /  |   |  __|
+       |  |__ /  /  |  |  /  / |_/ | |   |  |__
+       |   __|   ____  | /  /      | |   |     |
+       \_____/__|    |_|/__/       |_|   |_____|
+
+       ____    _      _     _____     _____
+      / _  \  | |    / /   | ____|   | __  \
+     | | | |  | |   / /    | |___    | |_| |
+     | | | |  | |  / /     |  ___|   |  _  /
+     | |_| |  | | / /      | |___    | | \ \
+      \___ /  |____/       |_____|   |_|  \_\
+
+      """
+       )
+input ("\n\nPress the enter key to exit!")
