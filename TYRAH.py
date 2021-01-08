26c7ebd27f7478579d5466d2ada534b36f98b93d
Tyrah <47400063+ursaturnine@users.noreply.github.com>
2021-01-08 14:08:22 -0800

    uploading my "tyrah" code
    Python print function practice! and keeping console open practice!

---
+++ TYRAH.py
@@ -0,0 +1,11 @@
+# Simple Name Program
+# a compsci challenge to write a program demonstrating the print function
+# by SaturnAlien
+# January, 08, 2021
+
+print ("Tyrah")
+
+# displays prompt "press the enter key to exit" to the user
+# program will wait for user to press "enter" to close window
+
+input ("\n\nPress the enter key to exit!")
