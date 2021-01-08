7274aef0c58b7d385c7d2965fe0a1c33ac245e96
Tyrah <47400063+ursaturnine@users.noreply.github.com>
2021-01-08 14:07:11 -0800

    uploading "my favorite quote" code
    Python coding print function practice!

---
+++ MY_FAVORITE_QUOTE.py
@@ -0,0 +1,13 @@
+# displays my favorite quote! a compsci challenge;
+# demonstrate print function and formatting
+# by SaturnAlien
+# January, 08 2021
+
+print ("Bitch, I know you know!")
+
+print ("-Rihanna, Pose")
+
+# displays prompt "press the enter key to exit" to the user
+# the program will wait for the user to press "enter" to close window
+
+input ("\n\nPress the enter key to exit!")
