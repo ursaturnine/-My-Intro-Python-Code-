32fefc76d85dc158fc1e3e6c4bf6e9086616fe7e
Tyrah <47400063+ursaturnine@users.noreply.github.com>
2021-01-08 14:05:53 -0800

    uploading my "game over" code
    Python practice problem

---
+++ GAME_OVER.py
@@ -0,0 +1,11 @@
+# Game Over
+# Demonstrates the print function
+# by SaturnAlien
+# January, 8 2021
+
+print ("Game Over")
+
+# displays prompt "Press the enter key to exit"
+# program waits for user to press "enter" key to close window
+
+input ("\n\nPress the enter key to exit.")
